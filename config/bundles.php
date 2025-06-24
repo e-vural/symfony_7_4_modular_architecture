748c61f9b2936578a9591b3f26e2b7a082b98c05
Mehmet Emre Vural <emre.vural@kodpit.com>
2025-06-24 15:01:27 +0300

    mercure bundle installed. docker configuration completed

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
